7265e9e35a2da78cbd7c7541163dc5a3532beee9
dlosch <318550+dlosch@users.noreply.github.com>
2025-11-10 15:08:56 +0100

    docs

--- sw.js
+++ sw.js
@@ -6,6 +6,11 @@ const urlsToCache = [
   './icon-192.png',
   './icon-512.png',
   './icon.png',
+  './docs/Argumente-Gemini25pro.md',
+  './docs/OPD-Gemini25pro.md',
+  './docs/Rhetorik-Checkliste-Gemini25pro.md',
+  './docs/Rhetorik-Gemini25pro.md',
+  './docs/index.json',
   '/marked.js' // Add marked.js to the cache
 ];
 
